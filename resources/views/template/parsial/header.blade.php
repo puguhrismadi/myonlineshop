7de449c8ffa626f2cb8d046cf01ad7ec12ca580f
= <=>
2020-09-08 15:51:48 +0700

    modifikasi front end template parsial

---
+++ resources/views/template/parsial/header.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,500">
+    <link rel="stylesheet" href="{{ asset('styles/style.css' )}}">
+    <script src="{{ asset('scripts/uikit.js')}}"></script>
+    <script src="{{ asset('scripts/uikit-icons.js')}}"></script>
+</head>

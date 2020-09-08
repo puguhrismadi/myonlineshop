5d7d72600e2ec22eebca27a15662184eb0ecc49a
= <=>
2020-09-08 14:10:08 +0700

    first commit

---
+++ resources/views/admin/page/welcomeadmin.blade.php
@@ -0,0 +1,6 @@
+@extends('admin.dashboard')
+@section('title',"Halaman Dashboard Admin")
+@section('content')
+    <h2>Welcome Admin</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero aut aperiam quasi quia officiis deleniti fugiat dignissimos cupiditate accusantium ipsa necessitatibus commodi est ipsum, ut dolore, nulla, laboriosam veniam molestiae!</p>
+@endsection
---
+++ resources/views/admin/partial/footer.blade.php
@@ -0,0 +1,8 @@
+
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+      <div class="copyright text-center my-auto">
+        <span>Copyright &copy; my-commerce 2020</span>
+      </div>
+    </div>
+  </footer>
